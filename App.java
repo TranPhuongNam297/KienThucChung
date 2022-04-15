8850f12765b0a0fc0478dace85f793b86449c335
TranPhuongNam297 <namtran0108642@gmail.com>
2022-04-15 14:39:56 +0700

    first commit

---
+++ App.java
@@ -0,0 +1,45 @@
+/// câu lệnh if else
+if (điều kiện)
+{
+    // code sẽ được thực thi nếu như đúng điều kiện
+} else
+{
+    // code sẽ được thực thi nếu như sai điều kiện
+}
+
+if (điều kiện 1)
+{
+    // code sẽ được thực thi nếu như đúng điều kiện 1
+}
+else if (điều kiện 2)
+{
+    // code sẽ được thực thi nếu như điều kiện 1 sai và điều kiện 2 đúng
+}
+else
+{
+    // code sẽ được thực thi nếu như điều kiện 1 và điều kiện 2 đều sai
+}
+
+/// câu lệnh while-loop
+while (điều kiện)
+{
+    // câu lệnh được thực thi 
+}
+
+do
+{
+    // câu lệnh được thực thi 
+}
+while (điều kiện);
+
+/// câu lệnh for-loop
+for (câu lệnh 1; câu lệnh 2; câu lệnh 3) {
+    // code được thực thi
+}
+
+for (type variableName : arrayName)
+{
+    // code được thực thi 
+}
+
+
